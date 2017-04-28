1b9fcffe178acfa788dfce09c63543dcb2fc85e6
Vincent Zhang <vincent_zhang95@hotmail.com>
2017-04-28 14:58:07 +1000

    assignment

---
+++ info1003-website/js/show-hide.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $("p.answer").hide();
+
+    // Question 1
+    $("#question1").click(function(){
+        $("#answer1").toggle();
+    });
+
+    // Question 2
+    $("#question2").click(function(){
+        $("#answer2").toggle();
+    });
+
+    // Question 3
+    $("#question3").click(function(){
+        $("#answer3").toggle();
+    });
+
+
+
+});
